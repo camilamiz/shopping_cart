670b1446ff9f6f201520c0e54bc24af7f6de0d90
Camila Mizumoto <camila.yuri.m@gmail.com>
2022-03-10 22:31:40 -0300

    structure product object (need refactor)

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,31 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def checkout
+    products_params.map do |param|
+      product = {
+        id: param['id'],
+        quantity: param['quantity'],
+        unit_amount: product(param['id'])['amount'],
+        total_amount: product(param['id'])['amount'] * param['quantity'],
+        discount: 10,
+        is_gift: product(param['id'])['is_gift']
+      }
+      byebug
+    end
   end
 
   private
 
+  def new_product
+    new_product ||= Product.new
+  end
+
+  def product(id)
+    new_product.find(id)
+  end
+
   def products_params
+    params.permit(products: [:id,:quantity])["products"]
   end
 end
