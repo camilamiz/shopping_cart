58fca13cbbcfc482ce10ea5af35c97442ea6d0c7
Camila Mizumoto <camila.yuri.m@gmail.com>
2022-03-12 14:06:53 -0300

    remove unused files

--- app/serializers/checkout_serializer.rb
+++
@@ -1,7 +0,0 @@
-class CheckoutSerializer < ActiveModel::Serializer
-  attributes
-    :total_amount,
-    :total_amount_with_discount,
-    :total_discount
-  has_many :products, serializer: ProductSerializer
-end
--- app/serializers/product_serializer.rb
+++
@@ -1,9 +0,0 @@
-class ProductSerializer < ActiveModel::Serializer
-  attributes
-    :id,
-    :quantity,
-    :unit_amount,
-    :total_amount,
-    :discount,
-    :is_gift
-end
