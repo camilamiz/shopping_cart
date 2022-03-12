7c27464f63f094836e10b2db66e3b532712536ff
Camila Mizumoto <camila.yuri.m@gmail.com>
2022-03-11 22:05:54 -0300

    add discount client and checkout result

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,21 +1,60 @@
 class Api::V1::ProductsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  # @client =
+
   def checkout
+    products = []
+
     products_params.map do |param|
-      product = {
-        id: param['id'],
+      id = param['id']
+
+      products << {
+        id: id,
         quantity: param['quantity'],
-        unit_amount: product(param['id'])['amount'],
-        total_amount: product(param['id'])['amount'] * param['quantity'],
-        discount: 10,
-        is_gift: product(param['id'])['is_gift']
+        unit_amount: product(id)['amount'],
+        total_amount: product(id)['amount'] * param['quantity'],
+        discount: discount(id),
+        is_gift: product(id)['is_gift']
       }
-      byebug
     end
+    checkout = purchase_resume(products)
+    checkout[:products] = products
+
+    render json: checkout, status: :created
   end
 
   private
+
+  def purchase_resume(products)
+    total_amount = 0
+    total_amount_with_discount = 0
+    total_discount = 0
+
+    products.map do|item|
+      total_amount += item[:total_amount]
+      total_amount_with_discount += item[:total_amount] * (1 - item[:discount])
+      total_discount += item[:total_amount] * item[:discount]
+    end
+
+    {
+      total_amount: total_amount,
+      total_amount_with_discount: total_amount_with_discount,
+      total_discount: total_discount
+    }
+  end
+
+  def discount(id)
+    begin
+      service = DiscountClientService.new
+      response = service.product_discount(id)
+      return response.percentage
+    rescue => exception
+      exception
+    end
+
+    return 0
+  end
 
   def new_product
     new_product ||= Product.new
