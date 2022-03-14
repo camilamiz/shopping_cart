3b8831cb6f3ccea6dc9614fb26b6c48b6c305ed2
Camila Mizumoto <camila.yuri.m@gmail.com>
2022-03-13 21:20:36 -0300

    fix if logic and change some names

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -10,27 +10,40 @@ class Api::V1::ProductsController < ApplicationController
   def checkout
     products = []
 
-    products_params.map do |param|
-      id = param['id']
-      total_amount = product(id)['amount'] * param['quantity']
+    if products_params.present?
+      products_params.map do |param|
+        id = param['id'].to_i
+        product = find_product(id)
 
-      products << {
-        id: id,
-        quantity: param['quantity'],
-        unit_amount: product(id)['amount'],
-        total_amount: total_amount,
-        discount: total_amount * discount(id),
-        is_gift: product(id)['is_gift']
-      }
-    end
+        if product.present?
+          quantity = param['quantity'].to_i
+          total_amount = product['amount'] * quantity
 
-    if validate_shopping_cart(products)
-      checkout = purchase_resume(products)
-      products << gift if is_black_friday?
-      checkout[:products] = products
+          products << {
+            id: id,
+            quantity: quantity,
+            unit_amount: product['amount'].to_i,
+            total_amount: total_amount.to_i,
+            discount: (total_amount * discount(id)).to_i,
+            is_gift: product['is_gift']
+          }
+        end
+      end
 
-      render json: checkout, status: :ok
-    end
+      if validate_shopping_cart(products)
+        checkout = purchase_resume(products)
+        if is_black_friday? then products << gift end
+        checkout[:products] = products
+
+        render json: checkout, status: :ok
+        return
+      end
+   else
+    render json: {
+      message: "The request body is invalid."
+    }, status: :unprocessable_entity
+    return
+   end
   end
 
   private
@@ -54,7 +67,7 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def validate_shopping_cart(products)
-    gifts = products.filter { |product| product[:is_gift] == true }
+    gifts = products.filter { |item| item[:is_gift] == true }
 
     if gifts.count > 0
       render json: {
@@ -97,7 +110,7 @@ class Api::V1::ProductsController < ApplicationController
     new_product ||= Product.new
   end
 
-  def product(id)
+  def find_product(id)
     new_product.find(id)
   end
 
