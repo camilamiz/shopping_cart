a904bfbbe9497524b72abb781fa26c8f5d7027bf
Camila Mizumoto <camila.yuri.m@gmail.com>
2022-03-18 18:41:51 -0300

    read .env file

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,10 @@ module ShoppingCart
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+      env_file = File.join(Rails.root, '.env')
+      File.open(env_file).read.split(/\n/).each do |string|
+        key_value = string.split('=')
+        ENV[key_value[0].to_s] = key_value[1]
       end if File.exists?(env_file)
     end
   end
